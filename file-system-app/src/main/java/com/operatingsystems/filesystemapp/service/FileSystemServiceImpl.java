ebd992ecee151e715ed9d7d0ff785a5cde3a4d66
Esteban Madrigal <ememadrigal.em@gmail.com>
2021-11-17 19:36:09 -0600

    FileSystem controller implemented

--- file-system-app/src/main/java/com/operatingsystems/filesystemapp/service/FileSystemServiceImpl.java
+++ file-system-app/src/main/java/com/operatingsystems/filesystemapp/service/FileSystemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.operatingsystems.filesystemapp.service;
 
+import com.operatingsystems.filesystemapp.handler.JSONUtils;
 import com.operatingsystems.filesystemapp.model.ActionResult;
 import com.operatingsystems.filesystemapp.model.PlainTextFile;
 import org.springframework.stereotype.Component;
@@ -30,6 +31,8 @@ public class FileSystemServiceImpl implements FileSystemService {
 
     @Override
     public ActionResult removeFile(final String username, final String fileId) {
+        var drive = JSONUtils.getFullDrive(username);
+        System.out.println(drive);
         return null;
     }
 
