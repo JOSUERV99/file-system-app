7b7d074fc98d7bb03cc3bf177c4a2070358d20c3
JOSUERV99 <josuerojasvega@gmail.com>
2021-11-21 18:56:07 -0600

    Tabs mode for rootDir and sharedWithMeDir

--- my-fs-frontend/src/components/FileSystemViewer.js
+++ my-fs-frontend/src/components/FileSystemViewer.js
@@ -1,46 +1,90 @@
-import React,  {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Tree from "@geist-ui/react/esm/tree";
-import { Alert } from "react-bootstrap";
+import { Alert, Tabs, Tab } from "react-bootstrap";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const color = {
     background: "#3d747e",
-}
+};
 
-const FileSystemViewer = ({global}) => {
+const OWN_MODE = 0,
+    SHARE_MODE = 1;
 
+const FileSystemViewer = ({ global }) => {
     const [glob, setGlobal] = global;
     const [path, setPath] = useState(glob.name);
 
-    useEffect(() => {
-        setGlobal({...glob, selectedItem: glob.drive.rootDir})
-        return () => {}
-    }, [])
+    const [mode, setMode] = useState(OWN_MODE);
+    const [selectedTab, setSelectedTab] = useState(mode == OWN_MODE ? "rootDir" : "sharedWithMeDir");
 
-    const setFSItem = (item) => { 
-        setGlobal({...glob, selectedItem : item }); 
-    }
+    useEffect(() => {
+        setGlobal({ ...glob, selectedItem: mode == OWN_MODE ? glob.drive.rootDir : glob.drive.sharedWithMeDir });
+        return () => { };
+    }, []);
+
+    const setFSItem = (item) => {
+        setGlobal({ ...glob, selectedItem: item });
+    };
 
     const handlePath = (p) => setPath(p);
-    const handelDrag = (e) => {alert(e)};
+    const handleDrag = (e) => {
+        alert(e);
+    };
 
-    const mapFile = (file) => <Tree.File id={file.id} name={file.name || '(No name specified)'} extra={file.bytesSize} onClick={() => setFSItem(file)}></Tree.File>;
+    const mapFile = (file) => (
+        <Tree.File
+            id={file.id}
+            name={file.name || "(No name specified)"}
+            extra={file.bytesSize}
+            onClick={() => setFSItem(file)}
+        ></Tree.File>
+    );
     const mapDir = (dir) => (
-        <Tree.Folder id={dir.id} name={dir.name } onClick={() => setFSItem(dir)}>
-            {dir.childrenDirectories.map(d => mapDir(d))}
-            {dir.files.map(f => mapFile(f))}
+        <Tree.Folder id={dir.id} name={dir.name} onClick={() => setFSItem(dir)}>
+            {dir.childrenDirectories.map((d) => mapDir(d))}
+            {dir.files.map((f) => mapFile(f))}
         </Tree.Folder>
-    )
+    );
 
     return (
-        <div style={{position: "fixed", x:0, y:0, height: "100%", zoom: "125%"}} className="mt-4 mx-4" >
-           
+        <div
+            style={{ position: "fixed", x: 0, y: 0, height: "100%", zoom: "125%" }}
+            className="mt-4 mx-4"
+        >
             <div className="mt-4"></div>
-            {
-                glob.selectedItem &&
-                <Alert variant='info'>{glob.selectedItem.name  || '(No name specified)'}</Alert>
-            }
-            <Tree initialExpand={true} onClick={handlePath}>{mapDir(glob.drive.rootDir)} </Tree>
-            
+
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={selectedTab}
+                onSelect={(k) => setSelectedTab(k)}
+                className="mb-3"
+            >
+                <Tab eventKey="rootDir" title="Home" >
+                    {glob.selectedItem && (
+                        <Alert variant="info">
+                            {glob.selectedItem.name || "(No name specified)"}
+                        </Alert>
+                    )}
+                    <Tree initialExpand={true} onClick={handlePath}>
+                        {mapDir(
+                            glob.drive.rootDir
+                        )}{" "}
+                    </Tree>
+                </Tab>
+                <Tab eventKey="sharedWithMe" title="Shared With Me">
+                {glob.selectedItem && (
+                        <Alert variant="info">
+                            {glob.selectedItem.name || "(No name specified)"}
+                        </Alert>
+                    )}
+                    <Tree initialExpand={true} onClick={handlePath}>
+                        {mapDir(
+                            glob.drive.sharedWithMeDir
+                        )}{" "}
+                    </Tree>
+                </Tab>
+            </Tabs>
+
         </div>
     );
 };
